d378045a52d0fd8d934813f35778645afb537951
henry-israel <henryisrael08@gmail.com>
2022-11-15 11:22:08 +0000

    fixes posterior hist filling

--- src/contours1D.cxx
+++ src/contours1D.cxx
@@ -10,13 +10,20 @@ contourBase(paramName+"_1DHist", massHierarchyOpt, octantOpt, burnin)
         throw;
     }
     TTree* oscTree = (TTree*)inFile->Get("osc_posteriors");
-
+	oscTree->SetBranchStatus("*", 1);
     TString histOptions = setHistOptions();
 
     posteriorHist = new TH1D( "posteriorHist", "posteriorHist", nBins, lowerBound, upperBound);
 
-    oscTree->Draw(paramName+">>posteriorHist+", histOptions);
-
+    oscTree->Draw(paramName+">>posteriorHist", histOptions);
+	if(!posteriorHist->GetEntries()){
+		std::cerr<<"ERROR : Posterior Histogram cannot be filled with entries from "<<paramName<<std::endl;
+		std::cerr<<"Acceptable entries are : "<<std::endl;
+		oscTree->Show();
+		throw;
+	}
+		
+	
     oscParamName=paramName;
     inFile->Close();
 
@@ -29,7 +36,7 @@ void contours1D::setup1DCredibleIntervals(){
     std::vector<double> credibleIntervals_vec = {0.67, 0.95, 99.9, 99.99, 99.99999};
     // {1sigma, 2sigma, 3sigma, 4sigma, 5sigma}
     double integral = posteriorHist->Integral();
-
+	
     for(int iCredibleInt=0; iCredibleInt<(int)credibleIntervals_vec.size(); iCredibleInt++){
         TString histName;
         double credVal = credibleIntervals_vec[iCredibleInt];
